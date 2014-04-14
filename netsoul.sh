f9984e9fa0181602c04d8a53c90e1518ae352021
kalatz_a <kalatz_a@epitech.eu>
2014-04-14 11:59:48 +0200

    replacing 10.42.6.6 with default gateway

--- netsoul.sh
+++ netsoul.sh
@@ -4,19 +4,22 @@
 # This script implies using a modified ns_auth
 #
 # Written By: Alpha14 (contact@alpha14.com)
-# Version: 1 (1-2014)
+# Version: 2 (4-2014)
+
+REMOTE_IP="8.8.4.4"
+GATEWAY=$(route -n|grep "UG"|grep -v "UGH"|cut -f 10 -d " ")
 
 function ping_ns {
-    if ping -q -c1 10.42.6.6 &> /dev/null
+    if ping -q -w 1 -c1 ${GATEWAY} &> /dev/null
     then
-	if ping -q -c1 8.8.8.8 &> /dev/null
+	if ping -q -w 1 -c1 ${REMOTE_IP} &> /dev/null
 	then
 	    ping_network
 	else
 	    ns_connect
 	fi
     else
-	echo "`date` : failed to contact netsoul gatway"
+	echo "`date` : failed to contact gateway"
 	sleep 1
 	ping_ns
     fi
@@ -25,15 +28,15 @@ function ping_ns {
 function ns_connect {
     echo "`date` : executing ns_auth"
     ns_auth
-    sleep 2
+    sleep 1
     ping_network
 }
 
 function ping_network {
-    if ping -q -c1 8.8.8.8 &> /dev/null
+    if ping -q -w 1 -c1 ${REMOTE_IP} &> /dev/null
     then
         echo "`date` : network up"
-	sleep 5
+	sleep 4
 	ping_network
     else
         echo "`date` : failed to contact network"
