76b0c496f4de6c0cffa597b68c59e27d7abefa3a
kalatz_a <kalatz_a@epitech.eu>
2014-04-24 20:10:40 +0200

    add function to wait for network at startup

--- netsoul.sh
+++ netsoul.sh
@@ -4,12 +4,31 @@
 # This script implies using a modified ns_auth
 #
 # Written By: Alpha14 (contact@alpha14.com)
-# Version: 2 (4-2014)
+# Version: 3 (4-2014)
 
 REMOTE_IP="8.8.4.4"
-GATEWAY=$(route -n|grep "UG"|grep -v "UGH"|cut -f 10 -d " ")
+WLANSTATE=$(iwconfig wlan0 | grep "Access Point" | cut -d: -f4)
+MSG=0
 
-function ping_ns {
+function network_wait
+{
+    GATEWAY=$(route -n|grep "UG"|grep -v "UGH"|cut -f 10 -d " ")
+    if [ -z "$GATEWAY" ]
+    then
+	if [ $MSG -ne 1 ]
+	then
+	    print "No network set"
+	fi
+	MSG=1
+	sleep 1
+	network_wait
+    else
+	ping_ns
+    fi
+}
+
+function ping_ns
+{
     if ping -q -w 1 -c1 ${GATEWAY} &> /dev/null
     then
 	if ping -q -w 1 -c1 ${REMOTE_IP} &> /dev/null
@@ -19,29 +38,44 @@ function ping_ns {
 	    ns_connect
 	fi
     else
-	echo "`date` : failed to contact gateway"
+        if [ $MSG -ne 3 ]
+	then
+	    print "Failed to contact gateway"
+	fi
+	MSG=3
 	sleep 1
 	ping_ns
     fi
 }
 
-function ns_connect {
-    echo "`date` : executing ns_auth"
+function ns_connect
+{
+    print "Executing ns_auth"
     ns_auth
     sleep 1
     ping_network
 }
 
-function ping_network {
+function ping_network
+{
     if ping -q -w 1 -c1 ${REMOTE_IP} &> /dev/null
     then
-        echo "`date` : network up"
+        if [ $MSG -ne 2 ]
+	then
+	    print "Network up"
+	fi
 	sleep 4
+	MSG=2
 	ping_network
     else
-        echo "`date` : failed to contact network"
+        print "Failed to contact network"
 	ping_ns
     fi
 }
 
-ping_ns
+function print
+{
+    logger "Netsoul: $1"
+}
+
+network_wait
